044e7dcafc042eb4950319f75241e563d360ce67
Michaela Adams <mjadams@fastmail.com>
2025-05-29 11:52:07 -0400

    Grid Life
    During the Hour of Hard Things today I worked on making this grid. I had tried
    previously to get a grid and a maze going in C++, but was immediately stumped by
    how c-stye strings work and also the trickiness of two dimensional arrays when
    working with pointers.
    
    Last night as I was reading my C++ book he walked through making a
    'SimpleString' class and that really unlocked some doors. Once I realized that I
    was just going to have to make my own SimpleString class with it's own 'append'
    and 'append_line' functions everything else started to make more
    sense. Additionally I let go of the two demensional array in favor of a single
    array and using two (actually 3) for loops to iterate over it (and then adding
    the col and row together to get the index of the cell I was working with).
    
    It was also very helpful to have worked out the problem in C# previously so I
    wasn't thinking about the langauge and the problem. I could just focus on the
    new things of the language.
    
    I still need to make a few more things dynamic (such as the size of the maze and
    the number of rows and columns and hwo the loops work). But I'm really pleased
    with how it looks and I think because I added in a check for the specific cell,
    making connected cells will fit in nicely.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,104 @@
-#include <cstdef>
-#include <unordered_map>
-#include <unordered_map>
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
 
-struct Cell {
-  Cell(int row, int col){
-    this.row = row;
-    this.col = col;
-    this.links = {};
+struct SimpleString {
+  SimpleString(size_t max_size) : max_size {max_size}, length{} {
+    if(max_size ==0){
+      throw std::runtime_error{"Max size must be at least 1."};
+    }
+    buffer = new char[max_size];
+    buffer[0] = 0; 
   }
-  void link(Cell cell, bool bidi){
-    this.links[cell] = true;
-    if(bidi){
-      cell.link(this, false);
+  ~SimpleString(){
+    delete[] buffer;
+  }
+
+  void print() {
+    printf("%s",  buffer);
+  }
+
+  // for appending a single character 
+  bool append(const char* x){
+    const auto x_len = strlen(x);
+
+    if(x_len + length + 2 > max_size) return false;
+
+    std::strncpy(buffer + length, x, max_size - length);
+
+    length += x_len;
+    buffer[length] = 0;
+
+    return true;
+  }
+
+  // for appending a whole line 
+  bool append_line(const char* x) {
+    const auto x_len = strlen(x);
+
+    if(x_len + length + 2 > max_size) return false;
+
+    // strncpy will copy up to num bytes from source into destination. once finished it will
+    // return the "destination", which is not used here.
+    // this is the signature
+    // char * std::strncpy(char* destination, const char* source, std::size_t num)
+    std::strncpy(buffer+length, x, max_size - length);
+
+    length += x_len;
+    buffer[length++] = '\n';
+    buffer[length] = 0;
+    return true;
+  }
+
+private:
+  size_t max_size;
+  char * buffer;
+  size_t length;
+  
+};
+
+struct Cell{
+  int nothing{0};
+};
+struct Grid{
+
+  Cell cells[25] = {};
+    
+  void draw(){
+    SimpleString maze{500};
+
+    maze.append_line("+--+--+--+--+--+");
+    for(int row = 0; row < 25; row += 5){
+
+      for(int col  = 0; col < 5; col += 1){
+        // holding the cells in a single demensional array
+        // makes some things easier, but also some things harder
+        // like thinking 
+        Cell cell = cells[row + col]; // this is really tripping my brain up
+
+        if(cell.nothing == 0){
+          maze.append("|  ");
+        }
+      }
+      maze.append_line("|");
+      for(int col =0; col < 5; col +=1){
+        Cell cell = cells[row + col];
+
+        if(cell.nothing == 0){
+          maze.append("+--");
+        }
+      }
+      maze.append_line("+");
     }
 
+    maze.print();
   }
-
-  void unlink(Cell cell, bool bidi){
-    // not sure how to "delete" an element from a map in C++
-    
-  }
+};
   
-  std::unordered_map<Cell, bool> links;
-  Cell north;
-  Cell south;
-  Cell east;
-  Cell west;
-private:
-  int row;
-  int col;
-}
-  
-struct Grid {
-  Grid(int row, int col){
-    this.row = row;
-    this.col = col;
-  }
-private:
-  int row;
-  int col;
-}
+int main(){
+  Grid grid;
 
-int main(){}
+  grid.draw();
+
+}
 
